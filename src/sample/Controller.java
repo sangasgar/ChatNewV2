4ed0ae2b4fe7ce736d93898173ebf3692dba2f83
Garegin <sangas@yandex.ru>
2020-08-30 01:54:50 +0300

    Сетевой чат готов

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,25 +2,43 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.Random;
 
 public class Controller {
+
     @FXML
-    public VBox vbox;
-    Random random = new Random();
-    public void onClickBtnHello(ActionEvent actionEvent) {
-        int x = random.nextInt(20) + 1;
-        Button button = new Button("Button" + x);
-        vbox.getChildren().add(button);
-        button.setOnAction(event -> {
-            System.out.println(x);
-            vbox.getChildren().remove(button);
-        });
+    TextField textfieldsms;
+    @FXML
+    TextArea textareasms;
+    @FXML
+    TextField nameuser;
+    @FXML
+    Label editMess;
+
+
+
+    public void sendMassageClick(ActionEvent actionEvent) {
+
+        textareasms.appendText("Пользователь " + nameuser.getText() + ": " + textfieldsms.getText() + "\n");
+        nameuser.requestFocus();
+
+    }
+
+    public void clearChat(ActionEvent actionEvent) {
+        textareasms.clear();
+    }
+
+    public void buttonClose(ActionEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Сетевой чат");
         primaryStage.setScene(new Scene(root, 700, 600));
         primaryStage.show();
     }
